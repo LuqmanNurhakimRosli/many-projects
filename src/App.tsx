dc10f8e090423292ce0af42df8341bdf46d54958
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-04 16:33:33 +0800

    trye

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { FaInstagram } from "react-icons/fa"
+import { FaWhatsapp } from "react-icons/fa6";
+import { CiFacebook } from "react-icons/ci";
+
+
 import Button from "./Components/Button"
 import './App.css'
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Button size='lg' className="green" onClick={() => alert('Coming soon...')} >
+      <Button size='lg'  variant='success'>
+      <FaWhatsapp 
+      className="text-xl hover:text-pink-500 transition-colors" 
+        />
+        <span className="ml-2">Whatsapp</span>
+      </Button>
+      <Button size='sm'  variant='warning'>
         <FaInstagram 
           className="text-xl hover:text-pink-500 transition-colors" 
         />
         <span className="ml-2">Instagram</span>
+      </Button>
+      <Button size='lg'  variant='danger'>
+      <CiFacebook 
+      className="text-xl hover:text-pink-500 transition-colors" 
+        />
+        <span className="ml-2">Facebook</span>
       </Button>
     </div>
   )
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,19 +1,22 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react'
+import classnames from 'classnames'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
   size?: 'sm' | 'lg'
-  className?: string
+  variant?: 'success' | 'warning' | 'danger'
 }
 
-function Button({ children, size = 'sm', className = '', ...rest }: ButtonProps) {
-  const sizeClass = size === 'lg' ? 'button-lg' : 'button-sm'
+function Button({ children, size, variant, className, ...rest }: ButtonProps) {
+  const buttonClasses = classnames(
+    'flex items-center px-4 py-2 bg-white rounded-lg shadow hover:shadow-md',
+    size && `button-${size}`,
+    variant && `button-${variant}`,
+    className
+  )
 
   return (
-    <button 
-      {...rest} 
-      className={`flex items-center px-4 py-2 bg-white rounded-lg shadow hover:shadow-md ${sizeClass} ${className}`}
-    >
+    <button {...rest} className={buttonClasses}>
       {children}
     </button>
   )
