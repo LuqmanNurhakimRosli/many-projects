cd540801354a2af7c13549f5d12b1cdb10aecae0
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-04 16:05:10 +0800

    mendoksai

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { FaInstagram } from "react-icons/fa"
 import Button from "./Components/Button"
-
+import './App.css'
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Button style={{color:'green'}} onClick={() => alert('Coming soon...')} >
+      <Button size='lg' className="green" onClick={() => alert('Coming soon...')} >
         <FaInstagram 
           className="text-xl hover:text-pink-500 transition-colors" 
         />
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,13 +2,17 @@ import { ButtonHTMLAttributes, ReactNode } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
+  size?: 'sm' | 'lg'
+  className?: string
 }
 
-function Button({ children, ...rest }: ButtonProps) {
+function Button({ children, size = 'sm', className = '', ...rest }: ButtonProps) {
+  const sizeClass = size === 'lg' ? 'button-lg' : 'button-sm'
+
   return (
     <button 
-      {...rest}
-      className="flex items-center px-4 py-2 bg-white rounded-lg shadow hover:shadow-md"
+      {...rest} 
+      className={`flex items-center px-4 py-2 bg-white rounded-lg shadow hover:shadow-md ${sizeClass} ${className}`}
     >
       {children}
     </button>
