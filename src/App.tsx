9bb7000ce955705432fa78b8ad39f95886e810fa
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-04 16:36:04 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,16 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <Button size='lg'  variant='success'>
       <FaWhatsapp 
-      className="text-xl hover:text-pink-500 transition-colors" 
         />
         <span className="ml-2">Whatsapp</span>
       </Button>
       <Button size='sm'  variant='warning'>
         <FaInstagram 
-          className="text-xl hover:text-pink-500 transition-colors" 
         />
         <span className="ml-2">Instagram</span>
       </Button>
       <Button size='lg'  variant='danger'>
       <CiFacebook 
-      className="text-xl hover:text-pink-500 transition-colors" 
         />
         <span className="ml-2">Facebook</span>
       </Button>
