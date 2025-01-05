6826feb9c5b93fdc859013a54c5c85f33f0f640f
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-05 08:53:05 +0800

    done

--- src/Components/Avatar.tsx
+++ src/Components/Avatar.tsx
@@ -1,8 +1,29 @@
-
+import { IoPersonSharp } from "react-icons/io5"
+ 
 function Avatar({src, alt, children}) {
   return (
     <div>
-     
+        if(src){
+            return (
+                <div className="avatar">
+                    <img src={src} alt={alt} />
+                </div>
+            )
+        }
+        if(src){
+            return (
+                <div className="avatar avatar-icon">
+                    {children}
+                </div>
+            )
+        }
+        else{
+            return (
+                <div className="avatar avatar-icon">
+                    <IoPersonSharp />
+                </div>
+            )
+        }
     </div>
   )
 }
