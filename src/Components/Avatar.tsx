163c2925fe084537839ae41e98b8d8f988f2868a
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-05 08:54:10 +0800

    fix ts

--- src/Components/Avatar.tsx
+++ src/Components/Avatar.tsx
@@ -1,29 +1,31 @@
 import { IoPersonSharp } from "react-icons/io5"
- 
-function Avatar({src, alt, children}) {
+
+interface AvatarProps {
+  src?: string
+  alt?: string
+  children?: React.ReactNode
+}
+
+function Avatar({ src, alt, children }: AvatarProps) {
+  if (src) {
+    return (
+      <div className="avatar">
+        <img src={src} alt={alt} />
+      </div>
+    )
+  }
+
+  if (children) {
+    return (
+      <div className="avatar avatar-icon">
+        {children}
+      </div>
+    )
+  }
+
   return (
-    <div>
-        if(src){
-            return (
-                <div className="avatar">
-                    <img src={src} alt={alt} />
-                </div>
-            )
-        }
-        if(src){
-            return (
-                <div className="avatar avatar-icon">
-                    {children}
-                </div>
-            )
-        }
-        else{
-            return (
-                <div className="avatar avatar-icon">
-                    <IoPersonSharp />
-                </div>
-            )
-        }
+    <div className="avatar avatar-icon">
+      <IoPersonSharp />
     </div>
   )
 }
