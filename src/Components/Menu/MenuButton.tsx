b67006f6a7558f1bb117b5c6ffe95439a126af32
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-06 11:58:59 +0800

    updaes

--- src/Components/Menu/MenuButton.tsx
+++ src/Components/Menu/MenuButton.tsx
@@ -1,4 +1,5 @@
 import Button from "../Button"
+import React from "react"
 
 function MenuButton({children, toggle}: {children: React.ReactNode, toggle: () => void}) {
   return (
--- src/Components/Menu/MenuDropDown.tsx
+++ src/Components/Menu/MenuDropDown.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 
 function MenuDropDown({children, open}: {children: React.ReactNode, open: boolean}) {
   return open ? (
